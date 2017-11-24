3535fd66b88e5a9282e75fdc134430ea951dea1f
Firas Chaaben <firaschaabencss@gmail.com>
2017-11-24 15:46:26 +0100

    change exemple task

--- task.py
+++ task.py
@@ -1,14 +1,18 @@
 import asyncio
 
-async def compute(x, y):
-    print("Compute %s + %s ..." % (x, y))
-    await asyncio.sleep(1.0)
-    return x + y
-
-async def print_sum(x, y):
-    result = await compute(x, y)
-    print("%s + %s = %s" % (x, y, result))
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number+1):
+        print("Task %s: Compute factorial(%s)..." % (name, i))
+        await asyncio.sleep(1)
+        f *= i
+    print("Task %s: factorial(%s) = %s" % (name, number, f))
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(print_sum(1, 2))
+loop.run_until_complete(asyncio.gather(
+    factorial("A", 2),
+    factorial("B", 3),
+    factorial("C", 4),
+))
 loop.close()
+
