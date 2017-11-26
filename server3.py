73f13c1aa9778d219fba4320b69cd6ad8ab4a786
Firas Chaaben <firaschaabencss@gmail.com>
2017-11-26 18:20:41 +0100

    add demo folder

--- server3.py
+++ server3.py
@@ -9,27 +9,19 @@ server_conx.listen(5)
 print("Server open on port {}".format(port))
 server_on = True
 customer_conx = []
-
 while server_on:
-
-
     request_con, wlist, xlist = select.select([server_conx],[], [], 0.05)
     for connexion in request_con:
         customer_link, infos_connexion = connexion.accept()
-
         customer_conx.append(customer_link)
-
     customers_data = []
     try:
         customers_data, wlist, xlist = select.select(customer_conx,[], [], 0.05)
     except select.error:
         pass
     else:
-
         for customer in customers_data:
-
             msg = customer.recv(1024)
-
             msg = msg.decode()
             print("Get {}".format(msg))
             customer.send(b"5 / 5")
