5782c399c13044c0352660da2ad7fc0cb9f95c0b
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-16 10:21:31 +0100

    update

--- demo/client.py
+++ demo/client.py
@@ -1,7 +1,7 @@
 import socket
 
 server_conx = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_conx.connect(('localhost', 12800))
+server_conx.connect(('0.0.0.0', 8008))
 
 msg = server_conx.recv(1024)
 print(msg)
